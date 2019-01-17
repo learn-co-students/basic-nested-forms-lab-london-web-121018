55dbee825e6766c1d63b5e9f60f25928d77e6002
github username <github email address>
2019-01-17 15:08:42 +0000

    learn submit

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,7 @@
 class RecipesController < ApplicationController
+
+
+
   def show
     @recipe = Recipe.find(params[:id])
   end
@@ -9,8 +12,26 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(name: 'salt')
+    @recipe.ingredients.build(name: 'pepper')
   end
 
   def create
+    @ingredients = []
+    params[:recipe][:ingredients_attributes].values.each do |obj|
+      @ingredients << Ingredient.find_or_create_by(obj)
+    end
+    # byebug
+    @recipe = Recipe.new(recipe_params(:title))
+    @recipe.ingredients << @ingredients
+    @recipe.save
+    # byebug
+    redirect_to @recipe
+  end
+
+  private
+
+  def recipe_params(*args)
+    params.require(:recipe).permit(args)
   end
 end
